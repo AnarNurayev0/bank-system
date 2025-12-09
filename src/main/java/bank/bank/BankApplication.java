3fb57dec9ea0e7921cb653d94d87347132f70456
Anar Nurayev <anarnurayev32@gmail.com>
2025-12-10 00:23:06 +0400

    Update project files

--- src/main/java/bank/bank/BankApplication.java
+++ src/main/java/bank/bank/BankApplication.java
@@ -9,5 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
+
     }
 }
